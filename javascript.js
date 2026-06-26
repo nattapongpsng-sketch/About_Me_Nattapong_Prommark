6184fde68c7e06b2c4ddd299a4f31a55b74e468e
Nattapong Prommark <nattapongpsng@gmail.com>
2026-06-26 22:04:26 +0700

    About Me Learning DOM

--- javascript.js
+++ javascript.js
@@ -1,44 +1,44 @@
 
 //ฟังก์ชันสำหรับปุ่มทักทาย
 function mybutton() {
-  document.getElementById("greeting").innerHTML = "Hello, I'm Nattapong Prommark";
+  document.getElementById("greeting").textContent = "Hello, I'm Nattapong Prommark";
 }
-//ฟังก
+//ฟังก์ชันสำหรับปุ่มเปลี่ยนโหมดพื้นหลัง
 function myfunction() {
-  if (document.body.style.backgroundColor === "black") {
+  if (document.body.style.backgroundColor == "black") {
     document.body.style.backgroundColor = "white";
     document.body.style.color = "black";
-    document.getElementById("mode").innerHTML = "โหมดพื้นหลังสีขาว";
+    document.getElementById("mode").textContent = "โหมดพื้นหลังสีขาว";
   } else {
   document.body.style.backgroundColor = "black";
   document.body.style.color = "white";
-  document.getElementById("mode").innerHTML = "โหมดพื้นหลังสีดำ";
+  document.getElementById("mode").textContent = "โหมดพื้นหลังสีดำ";
 }
 }
 //ฟังก์ชันสำหรับปุ่มนับจำนวนการคลิก
 function mycount() {
-  let count = parseInt(document.getElementById("count").innerHTML) || 0;
+  let count = parseInt(document.getElementById("count").textContent) || 0;
     count++;
-  document.getElementById("count").innerHTML = count;
+  document.getElementById("count").textContent = count;
 }
 
 const url = "https://api.open-meteo.com/v1/forecast?latitude=8.570949372392178&longitude=99.2511614163022&current=temperature_2m,is_day,rain,cloud_cover,weather_code,wind_speed_10m,wind_direction_10m&timezone=Asia%2FBangkok&forecast_days=1"
 document.getElementById("gettemp").addEventListener("click", fetchData);
 //ฟังก์ชันสำหรับดึงข้อมูลอุณหภูมิจาก API
 async function fetchData() {
-    document.getElementById("weather").innerHTML = "กำลังโหลด..."
+    document.getElementById("weather").innerHTML = "<b style='color: red;'>กำลังโหลด...</b>";
   try {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
-    document.getElementById("weather").innerHTML = "อุณหภูมิ: " + data.current.temperature_2m + " °C" + " | " + "ความเร็วลม: " + data.current.wind_speed_10m + " m/s" + " | " + "ปริมาณฝน: " + data.current.rain + " mm";
+    document.getElementById("weather").textContent = "วัน-เวลา " + data.current.time + " น."+" | " + "อุณหภูมิ: " + data.current.temperature_2m + " °C" + " | " + "ความเร็วลม: " + data.current.wind_speed_10m + " m/s" + " | " + "ปริมาณฝน: " + data.current.rain + " mm";
   } catch (error) {
     console.error("Error fetching data:", error);
-    document.getElementById("weather").innerHTML = "Error fetching data";
+    document.getElementById("weather").textContent = "Error fetching data";
   }
-};
+}
 function toggleSection() {
-  if (document.querySelector('.hidesection').style.display === "block") {
+  if (document.querySelector('.hidesection').style.display == "block") {
     document.querySelector('.hidesection').style.display = "none";
   } else {
     document.querySelector('.hidesection').style.display = "block";
